93e45167422d59a219469d936356f53caa664054
sergiohiroyuki <82911850+sergiohiroyuki@users.noreply.github.com>
2026-04-24 17:30:53 +0000

    makes `database.js` `client.connect()` more robust to error

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,7 +6,6 @@ test("GET to api/v1/status should retunr 200", async () => {
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-
   expect(responseBody.database.version).toEqual("16.13");
   expect(responseBody.database.max_connections).toEqual(100);
   expect(responseBody.database.opened_connections).toEqual(1);
